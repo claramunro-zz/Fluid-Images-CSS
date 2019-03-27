52021ec102d0fe9d61bdace5741bed57403b83d7
Guest User <Guest@epicodus-7.local>
2019-03-27 15:24:03 -0700

    minify html & js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 
 module.exports = {
 
@@ -50,8 +52,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       inject: 'body',
       template: './src/index.html',
-      filename: 'index.html'
-    })
+      filename: 'index.html',
+      minify: {
+      removeComments: true,
+      collapseWhitespace: true
+    }
+    }),
+
+    new UglifyJsPlugin(),
   ]
 
 };
